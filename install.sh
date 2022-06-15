f817f73ed6e3aeb013edf889fe1c8fa924cbf58f
Arik Maor <arik.maor@tensorleap.ai>
2022-06-16 00:08:51 +0300

    Report installation status

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
 set -euo pipefail
 
+INSTALL_ID=$RANDOM$RANDOM
+UNAME=$(uname -a)
+curl -s -XPOST https://us-central1-tensorleap-ops3.cloudfunctions.net/demo-contact-bot -H 'Content-Type: application/json' -d "{\"type\":\"install-script-init\",\"installId\":\"$INSTALL_ID\",\"uname\":\"$UNAME\"}" &> /dev/null &
+
 # Install k3d
 echo Checking k3d installation
 if !(k3d version);
 then
   echo Installing k3d...
+  curl -s -XPOST https://us-central1-tensorleap-ops3.cloudfunctions.net/demo-contact-bot -H 'Content-Type: application/json' -d "{\"type\":\"install-script-install-k3d\",\"installId\":\"$INSTALL_ID\"}" &> /dev/null &
   curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 fi
 
 if !(docker container list &> /dev/null);
 then
+  curl -s -XPOST https://us-central1-tensorleap-ops3.cloudfunctions.net/demo-contact-bot -H 'Content-Type: application/json' -d "{\"type\":\"install-script-docker-not-running\",\"installId\":\"$INSTALL_ID\"}" &> /dev/null &
   echo Docker is not running!
   exit -1
 fi
@@ -24,13 +30,16 @@ then
   INSTALLED_CHART_VERSION=$(docker exec -it k3d-tensorleap-server-0 kubectl get -n kube-system HelmChart tensorleap -o jsonpath='{.spec.version}')
   if [ "$LATEST_CHART_VERSION" == "$INSTALLED_CHART_VERSION" ]
   then
+    curl -s -XPOST https://us-central1-tensorleap-ops3.cloudfunctions.net/demo-contact-bot -H 'Content-Type: application/json' -d "{\"type\":\"install-script-up-to-date\",\"installId\":\"$INSTALL_ID\",\"version\":\"$LATEST_CHART_VERSION\"}" &> /dev/null &
     echo Installation in up to date!
     exit 0
   fi
 
+  curl -s -XPOST https://us-central1-tensorleap-ops3.cloudfunctions.net/demo-contact-bot -H 'Content-Type: application/json' -d "{\"type\":\"install-script-update-started\",\"installId\":\"$INSTALL_ID\",\"from\":\"$INSTALLED_CHART_VERSION\",\"to\":\"$LATEST_CHART_VERSION\"}" &> /dev/null &
   echo Installed Version: $INSTALLED_CHART_VERSION
   echo Updating to latest version...
   docker exec -it k3d-tensorleap-server-0 kubectl patch -n kube-system  HelmChart/tensorleap --type='merge' -p "{\"spec\":{\"version\":\"$LATEST_CHART_VERSION\"}}"
+  curl -s -XPOST https://us-central1-tensorleap-ops3.cloudfunctions.net/demo-contact-bot -H 'Content-Type: application/json' -d "{\"type\":\"install-script-update-success\",\"installId\":\"$INSTALL_ID\",\"from\":\"$INSTALLED_CHART_VERSION\",\"to\":\"$LATEST_CHART_VERSION\"}" &> /dev/null &
   echo 'Done! (note that images could still be downloading in the background...)'
 else
   # Get port and volume mount
@@ -78,23 +87,29 @@ metadata:
 EOF
 
   echo Creating tensorleap k3d cluster...
+  curl -s -XPOST https://us-central1-tensorleap-ops3.cloudfunctions.net/demo-contact-bot -H 'Content-Type: application/json' -d "{\"type\":\"install-script-creating-cluster\",\"installId\":\"$INSTALL_ID\",\"version\":\"$LATEST_CHART_VERSION\",\"volume\":\"$VOLUME\"}" &> /dev/null &
   k3d cluster create tensorleap \
     --k3s-arg='--disable=traefik@server:0' \
     -p "$PORT:80@loadbalancer" \
     -v $HOME/.config/tensorleap/manifests/tensorleap.yaml:/var/lib/rancher/k3s/server/manifests/tensorleap.yaml $VOLUMES_MOUNT_PARAM
 
   echo 'Waiting for images to download and install... (This can take up to 15 minutes depends on network speed)'
+  curl -s -XPOST https://us-central1-tensorleap-ops3.cloudfunctions.net/demo-contact-bot -H 'Content-Type: application/json' -d "{\"type\":\"install-script-helm-install-wait\",\"installId\":\"$INSTALL_ID\",\"version\":\"$LATEST_CHART_VERSION\"}" &> /dev/null &
   if !(docker exec -it k3d-tensorleap-server-0 kubectl wait --for=condition=complete --timeout=25m -n kube-system job helm-install-tensorleap);
   then
+    curl -s -XPOST https://us-central1-tensorleap-ops3.cloudfunctions.net/demo-contact-bot -H 'Content-Type: application/json' -d "{\"type\":\"install-script-helm-install-timeout\",\"installId\":\"$INSTALL_ID\",\"version\":\"$LATEST_CHART_VERSION\"}" &> /dev/null &
     echo "Timeout! Images may still be downloading, wait a few minutes and see if Tensorleap is available on http://127.0.0.1:$PORT If it's not, contact support"
     exit -1
   fi
   echo 'Waiting for containers to initialize... (Just a few more minutes!)'
+  curl -s -XPOST https://us-central1-tensorleap-ops3.cloudfunctions.net/demo-contact-bot -H 'Content-Type: application/json' -d "{\"type\":\"install-script-deployment-wait\",\"installId\":\"$INSTALL_ID\",\"version\":\"$LATEST_CHART_VERSION\"}" &> /dev/null &
   if !(docker exec -it k3d-tensorleap-server-0 kubectl wait --for=condition=available --timeout=25m -n tensorleap deploy -l app.kubernetes.io/managed-by=Helm);
   then
+    curl -s -XPOST https://us-central1-tensorleap-ops3.cloudfunctions.net/demo-contact-bot -H 'Content-Type: application/json' -d "{\"type\":\"install-script-deployment-timeout\",\"installId\":\"$INSTALL_ID\",\"version\":\"$LATEST_CHART_VERSION\"}" &> /dev/null &
     echo "Timeout! Images may still be downloading, wait a few minutes and see if Tensorleap is available on http://127.0.0.1:$PORT If it's not, contact support"
     exit -1
   fi
 
+  curl -s -XPOST https://us-central1-tensorleap-ops3.cloudfunctions.net/demo-contact-bot -H 'Content-Type: application/json' -d "{\"type\":\"install-script-install-success\",\"installId\":\"$INSTALL_ID\",\"version\":\"$LATEST_CHART_VERSION\"}" &> /dev/null &
   echo Tensorleap demo installed! It should be available now on http://127.0.0.1:$PORT
 fi
