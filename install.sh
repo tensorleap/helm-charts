228f335057d523342deab93c6e94df5fd87ca644
yotamazriel <yotamazriel@users.noreply.github.com>
2023-03-08 18:09:14 +0000

    Update static files

--- install.sh
+++ install.sh
@@ -293,7 +293,11 @@ image: $GPU_IMAGE
   runtime:\\
     gpuRequest: all
 "
-    GPU_ENGINE_VALUES='gpu: true'
+  fi
+
+  if [ "$USE_LOCAL_HELM" == "true" ]
+  then
+    sed_script="$sed_script;/- volume: ${VAR_DIR//\//\\/}\/manifests\/tensorleap\.yaml/,+2d"
   fi
 
   $HTTP_GET https://raw.githubusercontent.com/tensorleap/helm-charts/$FILES_BRANCH/config/k3d-config.yaml | \
@@ -306,8 +310,6 @@ function create_helm_values_file() {
   ${VOLUME_ENGINE_VALUES}
   ${GPU_ENGINE_VALUES}" \
     > $VAR_DIR/manifests/helm-values.yaml
-
-  echo --- > $VAR_DIR/manifests/tensorleap.yaml
 }
 function download_and_patch_helm_chart_manifest() {
   local sed_script="/targetNamespace:/ a\\
