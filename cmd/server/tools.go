ea3a25ee3a285332afb0131da6fa3ff47f353895
Moshe Kabala <moshe.kabala@tensorleap.ai>
2023-09-04 11:25:30 +0300

    Create the tools cmd with dedeceted function for reuse

--- cmd/server/tools.go
+++ cmd/server/tools.go
@@ -6,14 +6,19 @@ import (
 	kubectl "k8s.io/kubectl/pkg/cmd"
 )
 
-var ToolsCmd = &cobra.Command{
-	Use:   "tools",
-	Short: "3rd party tools to help manage local environment",
-	Long:  `3rd party tools to help manage local environment`,
+func NewToolCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "tools",
+		Short: "3rd party tools to help manage local environment",
+		Long:  `3rd party tools to help manage local environment`,
+	}
+
+	cmd.AddCommand(k3d.NewCmdK3d())
+	cmd.AddCommand(kubectl.NewDefaultKubectlCommand())
+
+	return cmd
 }
 
 func init() {
-	ToolsCmd.AddCommand(k3d.NewCmdK3d())
-	ToolsCmd.AddCommand(kubectl.NewDefaultKubectlCommand())
-	RootCommand.AddCommand(ToolsCmd)
+	RootCommand.AddCommand(NewToolCmd())
 }
