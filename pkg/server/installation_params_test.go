bd0f0651cff0e1623973b5fc4eea0ad4a60c3461
Moshe Kabala <moshe.kabala@tensorleap.ai>
2024-01-25 16:31:28 +0200

    Add option to select gpu

---
+++ pkg/server/installation_params_test.go
@@ -0,0 +1,37 @@
+package server
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetCreateK3sClusterParams(t *testing.T) {
+	t.Run("GpuDevices with all", func(t *testing.T) {
+		params := InstallationParams{
+			UseGpu:     true,
+			GpuDevices: "all",
+		}
+		createK3sClusterParams := params.GetCreateK3sClusterParams()
+		assert.Equal(t, createK3sClusterParams.GpuRequest, "all")
+	})
+
+	t.Run("GpuDevices with 1", func(t *testing.T) {
+		params := InstallationParams{
+			UseGpu: true,
+			GpuDevices: "1",
+		}
+		createK3sClusterParams := params.GetCreateK3sClusterParams()
+		assert.Equal(t, createK3sClusterParams.GpuRequest, "count=1")
+	})
+
+	t.Run("GpuDevices with GPU-0,GPU-1", func(t *testing.T) {
+		params := InstallationParams{
+			UseGpu: true,
+			GpuDevices: "GPU-0,GPU-1",
+		}
+		createK3sClusterParams := params.GetCreateK3sClusterParams()
+		assert.Equal(t, createK3sClusterParams.GpuRequest, "devices=GPU-0,GPU-1")
+	})
+
+}
