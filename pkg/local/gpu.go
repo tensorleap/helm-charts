a72e00811e92400e39a9be47347c2005efe7c3d1
Moshe Kabala <moshe.kabala@tensorleap.ai>
2024-01-28 17:07:14 +0200

    Add index to the gpu

--- pkg/local/gpu.go
+++ pkg/local/gpu.go
@@ -5,12 +5,23 @@ import (
 	"fmt"
 	"os/exec"
 	"runtime"
+	"strconv"
 	"strings"
 
 	"github.com/tensorleap/helm-charts/pkg/log"
 )
 
-func CheckNvidiaGPU() ([]string, error) {
+type GPU struct {
+	Index int
+	Name  string
+	ID    string
+}
+
+func (g GPU) String() string {
+	return fmt.Sprintf("GPU %d: %s (%s)", g.Index, g.Name, g.ID)
+}
+
+func CheckNvidiaGPU() ([]GPU, error) {
 	log.Info("Check for NVIDIA GPU")
 	var cmd *exec.Cmd
 	switch os := runtime.GOOS; os {
@@ -42,15 +53,26 @@ func CheckNvidiaGPU() ([]string, error) {
 	fmt.Printf("NVIDIA Driver Version: %s\n", driverVersion)
 
 	// List NVIDIA GPUs
-	cmd = exec.Command("nvidia-smi", "--query-gpu=name", "--format=csv,noheader")
+	cmd = exec.Command("nvidia-smi", "--query-gpu=index,name,uuid", "--format=csv,noheader")
 	listOutput, err := cmd.Output()
 	if err != nil {
 		return nil, fmt.Errorf("error listing NVIDIA GPUs: %s", err)
 	}
-	gpus := []string{}
+	gpus := []GPU{}
 	scanner := bufio.NewScanner(strings.NewReader(string(listOutput)))
 	for scanner.Scan() {
-		gpus = append(gpus, strings.TrimSpace(scanner.Text()))
+		line := scanner.Text()
+		fields := strings.Split(line, ", ")
+		index, err := strconv.Atoi(fields[0])
+		if err != nil {
+			return nil, fmt.Errorf("error parsing GPU index: %s", err)
+		}
+		gpu := GPU{
+			Index: index,
+			Name:  fields[1],
+			ID:    fields[2],
+		}
+		gpus = append(gpus, gpu)
 	}
 	if err := scanner.Err(); err != nil {
 		return nil, fmt.Errorf("error scanning NVIDIA GPU list: %s", err)
