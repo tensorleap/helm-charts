1c5d0e74d75ce1c540c463ac7ab3de02343e6486
Yuval Feldman <feldmanyuval@gmail.com>
2024-03-07 15:34:03 +0200

    PR comments

--- pkg/k3d/utils.go
+++ pkg/k3d/utils.go
@@ -7,6 +7,33 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+// CreateMirrorFromManifest creates a mirror config from a manifest
+// Example:
+// mirrors:
+//
+//	registry.k8s.io:
+//	  endpoint:
+//		  - http://k3d-tensorleap-registry:5000
+//	docker.io:
+//	  endpoint:
+//	    - http://k3d-tensorleap-registry:5000
+//	k8s.gcr.io:
+//	  endpoint:
+//	    - http://k3d-tensorleap-registry:5000
+//	gcr.io:
+//	  endpoint:
+//	    - http://k3d-tensorleap-registry:5000
+//	docker.elastic.co:
+//	  endpoint:
+//	    - http://k3d-tensorleap-registry:5000
+//	quay.io:
+//	  endpoint:
+//	    - http://k3d-tensorleap-registry:5000
+//	us-central1-docker.pkg.dev:
+//	  endpoint:
+//	    - http://k3d-tensorleap-registry:5000
+//	        `
+
 func CreateMirrorFromManifest(mfs *manifest.InstallationManifest, registryUrl string) (string, error) {
 	images := mfs.GetRegisterImages()
 	mirrors := make(map[string]interface{})
