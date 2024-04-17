8cfdfea171bb3ea2ef227873f4eb0b5803d8ccb0
Moshe Kabala <moshe.kabala@tensorleap.ai>
2024-04-17 13:50:23 +0300

    Add option to warp the uninstall

--- cmd/server/uninstall.go
+++ cmd/server/uninstall.go
@@ -14,29 +14,38 @@ func NewUninstallCmd() *cobra.Command {
 		Short: "Remove local Tensorleap installation",
 		Long:  `Remove local Tensorleap installation`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			log.SetCommandName("uninstall")
-			log.SendCloudReport("info", "Starting uninstall", "Starting", &map[string]interface{}{"args": args})
-			close, err := local.SetupInfra("uninstall")
+			previousDataDir := local.DEFAULT_DATA_DIR // we are not saving the previous data dir
+			err := local.SetDataDir(previousDataDir, "")
 			if err != nil {
 				return err
 			}
-			defer close()
-
-			ctx := cmd.Context()
-			err = server.Uninstall(ctx, purge)
-			if err != nil {
-				log.SendCloudReport("error", "Failed to uninstall", "Failed", &map[string]interface{}{"error": err.Error()})
-				return err
-			}
-
-			log.SendCloudReport("info", "Successfully completed uninstall", "Success", nil)
-			return nil
+			return RunUninstallCmd(cmd, purge)
 		},
 	}
 
 	cmd.Flags().BoolVar(&purge, "purge", false, "Remove all data and cached files")
 
 	return cmd
+}
+
+func RunUninstallCmd(cmd *cobra.Command, purge bool) error {
+	log.SetCommandName("uninstall")
+	log.SendCloudReport("info", "Starting uninstall", "Starting", &map[string]interface{}{"purge": purge})
+	close, err := local.SetupInfra("uninstall")
+	if err != nil {
+		return err
+	}
+	defer close()
+
+	ctx := cmd.Context()
+	err = server.Uninstall(ctx, purge)
+	if err != nil {
+		log.SendCloudReport("error", "Failed to uninstall", "Failed", &map[string]interface{}{"error": err.Error()})
+		return err
+	}
+
+	log.SendCloudReport("info", "Successfully completed uninstall", "Success", nil)
+	return nil
 }
 
 func init() {
