2811a2d01242cff7611f8ac9b000d3aec2c1783d
Asaf Yehezkel <asafyehezkel@Asafs-MacBook-Pro.local>
2025-09-15 15:07:43 +0300

    Expose kubectl only for k3d-tensorleap cluster

--- cmd/server/tools.go
+++ cmd/server/tools.go
@@ -1,8 +1,12 @@
 package server
 
 import (
+	"os"
+
 	k3d "github.com/k3d-io/k3d/v5/cmd"
 	"github.com/spf13/cobra"
+	"github.com/tensorleap/helm-charts/pkg/server"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
 	kubectl "k8s.io/kubectl/pkg/cmd"
 )
 
@@ -14,9 +18,26 @@ func NewToolCmd() *cobra.Command {
 	}
 
 	cmd.AddCommand(k3d.NewCmdK3d())
-	cmd.AddCommand(kubectl.NewDefaultKubectlCommand())
+	cmd.AddCommand(newTensorleapKubectlCommand())
 
 	return cmd
+}
+
+func newTensorleapKubectlCommand() *cobra.Command {
+	configFlags := genericclioptions.NewConfigFlags(true)
+	kubeContext := server.KUBE_CONTEXT
+	configFlags.Context = &kubeContext
+
+	validPluginPrefixes := []string{"kubectl"}
+
+	kubectlCmd := kubectl.NewKubectlCommand(kubectl.KubectlOptions{
+		PluginHandler: kubectl.NewDefaultPluginHandler(validPluginPrefixes),
+		Arguments:     os.Args,
+		ConfigFlags:   configFlags,
+		IOStreams:     genericclioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr},
+	})
+
+	return kubectlCmd
 }
 
 func init() {
