1b32f56365ef868a49b7bfc0b1d6db325f4eae32
Moshe Kabala <moshe.kabala@tensorleap.ai>
2025-11-12 18:43:37 +0200

    fix installation failure do to many releases

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -7,7 +7,7 @@ import (
 
 // When upgrading major number(the middle digit) it will require reinstall
 
-const Version = "v0.5.9"
+const Version = "v0.5.10"
 
 func IsMinorVersionSmaller(currentVersion, comperedVersion string) bool {
 
