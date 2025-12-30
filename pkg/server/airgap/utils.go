1891231d93b5de4ab0323ab07e21663ed5205e0c
Omri Yonatani <omri.yonatani@tensorleap.ai>
2025-12-30 10:55:40 +0200

    fix index branch

--- pkg/server/airgap/utils.go
+++
@@ -1,8 +0,0 @@
-package airgap
-
-const (
-	IMAGES_FILE_NAME            = "images.tgz"
-	MANIFEST_FILE_NAME          = "manifest.yaml"
-	SERVER_HELM_CHART_FILE_NAME = "tensorleap-chart.tgz"
-	INFRA_HELM_CHART_FILE_NAME  = "tensorleap-infra-chart.tgz"
-)
--- terraform/terraform-config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  required_version = "~> 1.2.0"
-  backend "gcs" {
-    bucket = "tensorleap-infra-nonprod"
-    prefix = "helm-charts-repository/"
-  }
-}
