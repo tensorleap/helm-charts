1837a41fd874277eee42849e903ace0c945bd1b5
Omri Yonatani <omri.yonatani@tensorleap.ai>
2026-01-05 15:50:22 +0200

    fix index branch

--- terraform/dns.tf
+++
@@ -1,8 +0,0 @@
-resource "google_dns_record_set" "github_pages_cname" {
-  name         = "helm.tensorleap.ai."
-  project      = "tensorleap-admin-3"
-  type         = "CNAME"
-  ttl          = 300
-  managed_zone = "tensorleap-ai"
-  rrdatas      = ["tensorleap.github.io."]
-}
