7879d17d514a4c9f6ec53014b3c8e62041af2ee3
Moshe Kabala <moshe.kabala@tensorleap.ai>
2026-01-26 11:25:49 +0200

    Update version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -7,7 +7,7 @@ import (
 
 // When upgrading major number(the middle digit) it will require reinstall
 
-const Version = "v0.9.2"
+const Version = "v0.9.3"
 
 func IsMinorVersionSmaller(currentVersion, comperedVersion string) bool {
 
