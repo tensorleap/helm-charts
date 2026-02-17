e7877dca06a3780b995f66bc455fa1175860e955
snirshemtov <snir.shemtov@tensorleap.ai>
2026-02-17 17:23:04 +0200

    version v0.9.6

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -7,7 +7,7 @@ import (
 
 // When upgrading major number(the middle digit) it will require reinstall
 
-const Version = "v0.9.5"
+const Version = "v0.9.6"
 
 func IsMinorVersionSmaller(currentVersion, comperedVersion string) bool {
 
