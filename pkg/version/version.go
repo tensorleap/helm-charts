32b98bd48a58be21062d3810726d50346545a33a
Asaf Yehezkel <asaf.yehezkel@tensorleap.ai>
2026-07-21 12:15:22 +0300

    Inc installer version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -7,7 +7,7 @@ import (
 
 // When upgrading major number(the middle digit) it will require reinstall
 
-const Version = "v0.10.11"
+const Version = "v0.10.13"
 
 func IsMinorVersionSmaller(currentVersion, comperedVersion string) bool {
 
