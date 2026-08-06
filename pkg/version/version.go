b49dd56ac07605730d73a7dbd9c0485e4fb25001
Omri Yonatani <omri.yonatani@tensorleap.ai>
2026-08-06 14:36:14 +0300

    Inc installer version

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -7,7 +7,7 @@ import (
 
 // When upgrading major number(the middle digit) it will require reinstall
 
-const Version = "v0.10.14"
+const Version = "v0.10.15"
 
 func IsMinorVersionSmaller(currentVersion, comperedVersion string) bool {
 
